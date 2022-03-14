393e38f22eb64f8ec942aad8d1eaba909a761c35
Gabriela SIlva <gabisantos.enf@gmail.com>
2022-03-14 11:12:02 +0100

    Javascript code

---
+++ designs.js
@@ -0,0 +1,29 @@
+let color = document.getElementById("colorPicker");
+let table = document.getElementById("pixelCanvas");
+let sizePicker = document.getElementById("sizePicker");
+
+let height = document.getElementById("inputHeight").value;
+let width = document.getElementById("inputWidth").value;
+makeGrid(height, width);
+
+sizePicker.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let height = document.getElementById("inputHeight").value;
+  let width = document.getElementById("inputWidth").value;
+  table.firstElementChild.remove();
+
+  makeGrid(height, width);
+});
+
+function makeGrid(height, width) {
+  for (let i = 0; i < height; i++) {
+    let row = table.insertRow(i);
+    for (let j = 0; j < width; j++) {
+      let cell = row.insertCell(j);
+      cell.addEventListener("click", (e) => {
+        cell.style.backgroundColor = color.value;
+      });
+    }
+  }
+}
