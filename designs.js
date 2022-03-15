fb089a68fe8600951b4934f2f0bb4406f9d3a621
Gabriela SIlva <gabisantos.enf@gmail.com>
2022-03-15 09:12:29 +0100

    Comments on JavaScript and code refine

--- designs.js
+++ designs.js
@@ -1,29 +1,36 @@
-let color = document.getElementById("colorPicker");
-let table = document.getElementById("pixelCanvas");
-let sizePicker = document.getElementById("sizePicker");
+let color = document.getElementById('colorPicker')
+let table = document.getElementById('pixelCanvas')
+let sizePicker = document.getElementById('sizePicker')
 
-let height = document.getElementById("inputHeight").value;
-let width = document.getElementById("inputWidth").value;
-makeGrid(height, width);
+const height = document.getElementById('inputHeight').value
+const width = document.getElementById('inputWidth').value
+makeGrid(height, width)
 
-sizePicker.addEventListener("click", (e) => {
-  e.preventDefault();
+sizePicker.addEventListener('click', e => {
+  // prevents the page from reloading on submitting the form
+  e.preventDefault()
 
-  let height = document.getElementById("inputHeight").value;
-  let width = document.getElementById("inputWidth").value;
-  table.firstElementChild.remove();
+  let height = inputHeight.value
+  let width = inputWidth.value
+  table.firstElementChild.remove()
 
-  makeGrid(height, width);
-});
+  // makes grid
+  makeGrid(height, width)
+})
 
-function makeGrid(height, width) {
+/**
+ * @desc create a grid of squares
+ * @param int $width - number of squares representing the width of the grid
+ * @param int $height - number of squares representing the height of the grid
+ */
+function makeGrid (height, width) {
   for (let i = 0; i < height; i++) {
-    let row = table.insertRow(i);
+    let row = table.insertRow(i)
     for (let j = 0; j < width; j++) {
-      let cell = row.insertCell(j);
-      cell.addEventListener("click", (e) => {
-        cell.style.backgroundColor = color.value;
-      });
+      let cell = row.insertCell(j)
+      cell.addEventListener('click', e => {
+        cell.style.backgroundColor = color.value
+      })
     }
   }
 }
